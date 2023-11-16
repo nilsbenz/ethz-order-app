31dbc6d5a46d4667c828e10ade9ebac332978d3f
nilsbenz <nils.benz@icloud.com>
2023-11-16 17:52:38 +0100

    chore: make the pwa on ios more beautiful

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,49 +1,50 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       includeAssets: [
-        'favicon.ico',
-        'apple-touch-icon-180x180.png',
-        'maskable-icon-512x512.png',
+        "favicon.ico",
+        "apple-touch-icon-180x180.png",
+        "maskable-icon-512x512.png",
       ],
       manifest: {
-        name: 'bstell.online',
-        short_name: 'bstell',
-        description: 'bstell.online',
-        display: 'standalone',
-        start_url: '.',
-        scope: '.',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
+        name: "bstell.online",
+        short_name: "bstell",
+        description: "bstell.online",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: ".",
+        scope: ".",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
         icons: [
           {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
           },
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
