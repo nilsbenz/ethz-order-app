0cd0d8adde565b48d660b56c5ecf982c93de9f6c
nilsbenz <nils.benz@icloud.com>
2023-11-24 09:41:54 +0100

    chore: refactor super admins section

---
+++ apps/webapp/src/components/admin/NewSuperAdminForm.tsx
@@ -0,0 +1,90 @@
+import { Collection } from "@/lib/collections";
+import { db, functions } from "@/lib/firebase";
+import { appUserConverter } from "@/lib/model/users";
+import { UserLevel } from "@order-app/types";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+  Input,
+} from "@order-app/ui";
+import { DialogClose } from "@order-app/ui/src/components/dialog";
+import { doc, updateDoc } from "firebase/firestore";
+import { httpsCallable } from "firebase/functions";
+import { PlusIcon } from "lucide-react";
+import { FormEvent, useRef, useState } from "react";
+
+export default function NewSuperAdminForm() {
+  const newSuperAdminInput = useRef<HTMLInputElement>(null);
+  const [newSuperAdminInputState, setNewSuperAdminInputState] = useState<
+    "idle" | "busy"
+  >("idle");
+  const [openDialog, setOpenDialog] = useState(false);
+
+  async function handleAddSuperAdmin(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      setNewSuperAdminInputState("busy");
+      const userId = newSuperAdminInput.current?.value;
+      if (!userId) {
+        throw "no userid provided";
+      }
+      await updateDoc(
+        doc(db, Collection.Users, userId).withConverter(appUserConverter),
+        { level: UserLevel.SuperAdmin }
+      );
+      await httpsCallable(
+        functions,
+        "updateSuperAdminStatus"
+      )({
+        userId,
+        isSuperAdmin: true,
+      });
+      newSuperAdminInput.current!.value = "";
+      setOpenDialog(false);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setNewSuperAdminInputState("idle");
+    }
+  }
+
+  return (
+    <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+      <DialogTrigger asChild>
+        <Button className="flex gap-1" size="sm">
+          Neu
+          <PlusIcon className="h-5" strokeWidth={2.25} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>Super Admin hinzufügen</DialogHeader>
+        <DialogDescription>
+          <form onSubmit={handleAddSuperAdmin} className="flex flex-col gap-4">
+            <Input
+              id="displayNameInput"
+              ref={newSuperAdminInput}
+              className="w-full"
+              disabled={newSuperAdminInputState === "busy"}
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="ghost">Abbrechen</Button>
+              </DialogClose>
+              <Button
+                type="submit"
+                disabled={newSuperAdminInputState === "busy"}
+              >
+                Hinzufügen
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ apps/webapp/src/components/admin/SuperAdminList.tsx
@@ -0,0 +1,108 @@
+import { Collection } from "@/lib/collections";
+import { db, functions } from "@/lib/firebase";
+import { appUserConverter } from "@/lib/model/users";
+import useAuthStore from "@/lib/store/auth";
+import { AppUser, UserLevel } from "@order-app/types";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "@order-app/ui";
+import { DialogClose } from "@order-app/ui/src/components/dialog";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { httpsCallable } from "firebase/functions";
+import { Trash2Icon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function SuperAdminList() {
+  const user = useAuthStore((state) => state.user);
+  const [superAdmins, setSuperAdmins] = useState<AppUser[]>();
+  const [busy, setBusy] = useState(false);
+
+  function handleRemoveSuperAdmin(userId: string) {
+    return async () => {
+      try {
+        setBusy(true);
+        await httpsCallable(
+          functions,
+          "updateSuperAdminStatus"
+        )({ userId, isSuperAdmin: false }).catch(() => {
+          alert("Ein Fehler ist aufgetreten!");
+        });
+        await updateDoc(
+          doc(db, Collection.Users, userId).withConverter(appUserConverter),
+          { level: UserLevel.User }
+        );
+      } finally {
+        setBusy(false);
+      }
+    };
+  }
+
+  useEffect(() => {
+    const superAdminsQuery = query(
+      collection(db, Collection.Users),
+      where("level", "==", 9)
+    ).withConverter(appUserConverter);
+    const unsubscribe = onSnapshot(superAdminsQuery, (snapshot) =>
+      setSuperAdmins(snapshot.docs.map((d) => d.data()))
+    );
+    return unsubscribe;
+  }, [user?.uid]);
+
+  return (
+    <div className="flex flex-col divide-y">
+      {superAdmins?.map((admin) => (
+        <div key={admin.id} className="flex items-center gap-2 py-1">
+          <p className="flex-grow whitespace-nowrap font-medium">
+            {admin.displayName}
+          </p>
+          <p className="truncate font-normal text-muted-foreground">
+            {admin.id}
+          </p>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                disabled={admin.id === user?.uid}
+              >
+                <Trash2Icon />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>Super Admin entfernen?</DialogHeader>
+              <DialogDescription>
+                Bist du dir sicher, dass du {admin.displayName} entfernen
+                möchtest?
+              </DialogDescription>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="ghost">Abbrechen</Button>
+                </DialogClose>
+                <Button
+                  variant="destructive"
+                  onClick={handleRemoveSuperAdmin(admin.id)}
+                  disabled={busy}
+                >
+                  Entfernen
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      ))}
+    </div>
+  );
+}
