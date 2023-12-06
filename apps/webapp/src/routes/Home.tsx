39282584c8b1fb5797c40e017a0420b83ff62a21
nilsbenz <nils.benz@icloud.com>
2023-12-06 11:00:35 +0100

    chore: update text on home page

--- apps/webapp/src/routes/Home.tsx
+++ apps/webapp/src/routes/Home.tsx
@@ -2,11 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4">
       <h2 className="h1">Hopp</h2>
-      <p>
-        Will da ganze no voll i da Entwickligsphasa isch sechsch do no nöd so
-        viel... Falls da aber no nöd gmacht hesch mosch no da QR Code bim
-        Druckergrät go scanna.
-      </p>
+      <p>Di gits no nünt...</p>
     </div>
   );
 }
